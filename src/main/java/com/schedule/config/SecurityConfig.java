cb39d8c8ad12aa0e31a9cb321c0c3d976f5cf001
phint85 <tranhoangphi0987@gmail.com>
2023-06-12 16:11:39 +0700

    add method to get user info by id or username

--- src/main/java/com/schedule/config/SecurityConfig.java
+++ src/main/java/com/schedule/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf().disable()
+			.cors().disable().csrf().disable()
 			.authorizeHttpRequests()
 			.antMatchers("/api/v1/auth/**")
 			.permitAll()
