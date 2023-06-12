96ad9cfd90cc5fa187c97e6c1ef1fad91182e3bd
phint85 <tranhoangphi0987@gmail.com>
2023-06-12 17:05:42 +0700

    add method to get user info by id or username

--- src/main/java/com/schedule/config/SecurityConfig.java
+++ src/main/java/com/schedule/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -72,7 +73,12 @@ public class SecurityConfig {
 			.permitAll()
 			.and()
 			.authenticationProvider(authenticationProvider());
-			
+			http
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+			.and()
+			.sessionManagement()
+			.sessionFixation().newSession();
 			return http.build();
 	}
 	
